1c21c772d152c61cb37c70fbb94fcaae4bc49dbf
80010-11-06 <80010-11-06@exchange.ad.afpanet>
2020-01-06 09:31:49 +0100

    update

---
+++ part 10 - tableaux/exo 1/assets/js/script.js
@@ -0,0 +1,16 @@
+function tab(){
+    valeur = prompt("Ecrivez "+ taille + " valeur pour le tableau :")
+    var tableau = [valeur];
+    var split = valeur.split(" ")
+    console.log(tableau)
+    
+
+}
+function tailletab(){
+    taille = prompt("Ecrivez la taille pour le tableau :")
+    console.log("Taille du tableau : " + taille)
+
+}
+
+tailletab();
+tab();
